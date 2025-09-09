388f83ceb0eaf0ddc1f00713c2407232cae7cb54
Vyom2663 <vyomjoshi2603@gmail.com>
2025-09-09 22:14:08 +0530

    bg image

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import SignupPage from "./task/sign-up";
+import background from "/public/background.png";
 
 function App() {
   return (
-    <div className="m-0 p-0 bg-no-repeat bg-cover" style={{ backgroundImage: "url('/public/background.png')" }}>
+    <div
+      className="m-0 p-0 bg-no-repeat bg-cover"
+      style={{ backgroundImage: `url(${background})` }}
+    >
       <SignupPage />
     </div>
   );
